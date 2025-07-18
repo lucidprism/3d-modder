ac5b487106d9dfeab2a2e2d9641e982332d11d99
LucidPrism <63215060+lucidprism@users.noreply.github.com>
2025-07-18 14:17:42 -0700

    Updated index.html added js folder and auth.js file

---
+++ assets/js/auth.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAY60GV85KGRnBNbp76yIkOujGzD9Llf4Q",
+  authDomain: "lucidprismauth.firebaseapp.com",
+  projectId: "lucidprismauth",
+  storageBucket: "lucidprismauth.firebasestorage.app",
+  messagingSenderId: "1009604111946",
+  appId: "1:1009604111946:web:ffe630acf3c22444fa80b3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
